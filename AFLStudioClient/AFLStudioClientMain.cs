1c0d9917b3b8b5dab070273ca6f0f12c26fdd9c7
AFL-Studio <127034041+AFL-Studio@users.noreply.github.com>
2023-03-11 05:17:00 +0000

    the great change

---
+++ AFLStudioClient/AFLStudioClientMain.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, the fucking Client World!");
+    }
+}
